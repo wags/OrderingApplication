90988a4948fe6d8c6d138071efb319ca2e542fc6
Matt Wagner <m.wagner@dwdiesel.com>
2017-01-25 14:11:46 -0500

    Build the form using HTML Helpers and model binding

---
+++ Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderingApplication.Models
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime? ContactDate { get; set; }
+        public string ClientType { get; set; }
+        public string Notes { get; set; }
+        public Address BillingAddress { get; set; }
+        public Address MailingAddress { get; set; }
+    }
+}
