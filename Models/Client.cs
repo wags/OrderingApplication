45578097b24d27c70d1a22cdcda6ab39f914d9e0
Matt Wagner <m.wagner@dwdiesel.com>
2017-01-27 10:46:14 -0500

    Add annotations for data validation

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,23 @@ namespace OrderingApplication.Models
 {
     public class Client
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string CompanyName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public DateTime? ContactDate { get; set; }
+        [Required]
         public string ClientType { get; set; }
+        [Required]
+        [MaxLength(25)]
         public string Notes { get; set; }
         public Address BillingAddress { get; set; }
         public Address MailingAddress { get; set; }
